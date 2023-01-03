6b3d530d9aed5d2e898348641e0b7a465c061da5
yukong <kyb062666@gmail.com>
2023-01-04 01:53:49 +0900

    chore: 프로젝트 초기 환경 셋팅

---
+++ src/main/java/com/dobugs/yologaapi/YologaApiApplication.java
@@ -0,0 +1,13 @@
+package com.dobugs.yologaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YologaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YologaApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dobugs/yologaapi/YologaApiApplicationTests.java
@@ -0,0 +1,12 @@
+package com.dobugs.yologaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YologaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
