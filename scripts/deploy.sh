41472f73bd18b752e43df072166994d7c7e39cef
yukong <92148749+kyukong@users.noreply.github.com>
2023-01-11 17:30:18 +0900

    chore: 배포 스크립트 생성 (#2)
    * chore: 배포 스크립트 생성
    
    * chore: 이전에 실행되던 스프링 프로젝트 port 죽이기

---
+++ scripts/deploy.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+APP_NAME=yologa-api
+JAR_NAME=yologa-api-0.0.1-SNAPSHOT.jar
+
+# 현재 실행중인 서버가 있으면 잡아서 종료
+CURRENT_PID=$(pgrep -f $APP_NAME)
+if [ -z $CURRENT_PID ]
+then
+  echo ">>>> java process not found."
+else
+  echo ">>>> PID: $CURRENT_PID kill."
+  kill -15 $CURRENT_PID
+  sleep 45
+fi
+
+# .jar 파일 java 실행
+echo "build project"
+cd ~
+cd $APP_NAME
+./gradlew bootJar
+
+# kill 8080 port
+fuser -k -n tcp 8080
+
+echo ">>>> $APP_NAME execute."
+cd build/libs
+nohup java -jar $JAR_NAME &
+
+sleep 20
+CURRENT_PID=$(pgrep -f $APP_NAME)
+echo ">>>> New PID: $CURRENT_PID"
