91f12d421ee2127d2c453e2dd4b5acdc76ba2339
yukong <kyb062666@gmail.com>
2023-01-13 02:31:13 +0900

    feat: 예외처리 추가

---
+++ src/main/java/com/dobugs/yologaapi/exception/dto/response/ExceptionResponse.java
@@ -0,0 +1,18 @@
+package com.dobugs.yologaapi.exception.dto.response;
+
+public class ExceptionResponse {
+
+    private final String message;
+
+    private ExceptionResponse(final String message) {
+        this.message = message;
+    }
+
+    public static ExceptionResponse from(final String message) {
+        return new ExceptionResponse(message);
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
