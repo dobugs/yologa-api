93d1b1bb0319215641bad1d5e0b889b974f95fb7
yukong <kyb062666@gmail.com>
2023-01-13 20:55:58 +0900

    refactor: JPA Audit 설정 클래스 분리

---
+++ src/main/java/com/dobugs/yologaapi/config/JpaAuditingConfiguration.java
@@ -0,0 +1,9 @@
+package com.dobugs.yologaapi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaAuditingConfiguration {
+}
