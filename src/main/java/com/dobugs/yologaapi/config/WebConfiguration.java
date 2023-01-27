4675d54dfdf6ecdbcf5d577bd4265293f615e6dc
yukong <kyb062666@gmail.com>
2023-01-27 19:04:41 +0900

    feat: CORS 허용

---
+++ src/main/java/com/dobugs/yologaapi/config/WebConfiguration.java
@@ -0,0 +1,17 @@
+package com.dobugs.yologaapi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+            .maxAge(3000);
+    }
+}
