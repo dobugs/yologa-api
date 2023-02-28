aca54d2d094b6c1472be664201595a6bc3864481
yukong <kyb062666@gmail.com>
2023-02-28 18:05:38 +0900

    refactor: 불필요한 코드 제거 및 와일드카드 사용

--- src/main/java/com/dobugs/yologaapi/repository/ParticipantRepository.java
+++ src/main/java/com/dobugs/yologaapi/repository/ParticipantRepository.java
@@ -11,13 +11,7 @@ import com.dobugs.yologaapi.repository.dto.response.ParticipantDto;
 public interface ParticipantRepository extends JpaRepository<Participant, Long> {
 
     @Query(
-        value = "SELECT m.id AS id, m.nickname AS nickname\n"
-            + "FROM participant AS p\n"
-            + "LEFT JOIN member AS m\n"
-            + "ON p.member_id = m.id\n"
-            + "WHERE p.running_crew_id = ?1 AND p.status = ?2 AND p.archived = true\n"
-            + ";",
-        countQuery = "SELECT (p.id)\n"
+        value = "SELECT *\n"
             + "FROM participant AS p\n"
             + "LEFT JOIN member AS m\n"
             + "ON p.member_id = m.id\n"
