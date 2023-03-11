89f27c8d5a37fe86086853452828b9f833f42c96
yukong <kyb062666@gmail.com>
2023-03-12 02:21:56 +0900

    feat: RestTemplate 설정 추가

--- src/main/java/com/dobugs/yologaapi/auth/support/Connector.java
+++ src/main/java/com/dobugs/yologaapi/auth/support/Connector.java
@@ -2,7 +2,9 @@ package com.dobugs.yologaapi.auth.support;
 
 import org.springframework.http.ResponseEntity;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 public interface Connector {
 
-    <T> ResponseEntity<T> post(Object request, Class<T> responseType);
+    <T> ResponseEntity<T> post(HttpServletRequest request, Class<T> responseType);
 }
--- src/main/java/com/dobugs/yologaapi/auth/support/DobugsConnector.java
+++ src/main/java/com/dobugs/yologaapi/auth/support/DobugsConnector.java
@@ -1,14 +1,20 @@
 package com.dobugs.yologaapi.auth.support;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.DefaultResponseErrorHandler;
 import org.springframework.web.client.RestTemplate;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Component
 public class DobugsConnector implements Connector {
 
-    private static final RestTemplate REST_TEMPLATE = new RestTemplate();
+    private final RestTemplate REST_TEMPLATE = new RestTemplate();
 
     private final String authHost;
     private final String authPath;
@@ -19,9 +25,30 @@ public class DobugsConnector implements Connector {
     ) {
         this.authHost = authHost;
         this.authPath = authPath;
+        config();
     }
 
-    public <T> ResponseEntity<T> post(final Object request, final Class<T> responseType) {
-        return REST_TEMPLATE.postForEntity(authHost + authPath, request, responseType);
+    public <T> ResponseEntity<T> post(final HttpServletRequest request, final Class<T> responseType) {
+        final String authorization = request.getHeader("Authorization");
+        return REST_TEMPLATE.postForEntity(
+            authHost + authPath,
+            createAuthorizationHeaders(authorization),
+            responseType
+        );
+    }
+
+    private HttpHeaders createAuthorizationHeaders(final String token) {
+        final HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", token);
+        return headers;
+    }
+
+    private void config() {
+        REST_TEMPLATE.setRequestFactory(new HttpComponentsClientHttpRequestFactory());
+        REST_TEMPLATE.setErrorHandler(new DefaultResponseErrorHandler() {
+            public boolean hasError(final ClientHttpResponse response) {
+                return false;
+            }
+        });
     }
 }
