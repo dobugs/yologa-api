bcee198dda65c01b9b2f3f88efac7ae16ba91f76
yukong <kyb062666@gmail.com>
2023-05-09 22:03:29 +0900

    chore: 스크립트 실행 시 profile dev 로 설정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,6 +4,10 @@ APP_NAME=yologa-api
 JAR_NAME=yologa-api-0.0.1-SNAPSHOT.jar
 LOG_FILE=application.log
 PORT=8080
+PROFILE="dev"
+
+echo -e "port : ${PORT}"
+echo -e "profile : ${PROFILE}"
 
 # 현재 실행중인 서버가 있으면 잡아서 종료
 CURRENT_PID=$(pgrep -f $APP_NAME)
@@ -27,7 +31,7 @@ fuser -k -n tcp ${PORT}
 
 echo ">>>> $APP_NAME execute."
 cd build/libs
-nohup java -jar $JAR_NAME >> ${LOG_FILE} 2>/dev/null &
+nohup java -jar ${JAR_NAME} --spring.profiles.active=${PROFILE} >> ${LOG_FILE} 2>/dev/null &
 
 sleep 20
 CURRENT_PID=$(pgrep -f $APP_NAME)
